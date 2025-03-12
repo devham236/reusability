020ba2c98ece851892308ffed5bc32659884fe71
Hamza Mirza <hamza665@hotmail.de>
2025-03-12 11:59:48 +0100

    adjusted types

--- google_notes/src/Context/GlobalContext.tsx
+++ google_notes/src/Context/GlobalContext.tsx
@@ -1,9 +1,37 @@
-import React, { createContext, ReactNode } from "react";
+import React, { createContext, ReactNode, useState } from "react";
+import { GlobalContextType, Note } from "./types/contextTypes";
 
-const GlobalContext = createContext();
+const GlobalContext = createContext<GlobalContextType>({
+  allNotes: [],
+  setAllNotes: () => {},
+  addNote: () => {},
+  deleteNote: () => {},
+  updateNote: () => {},
+});
 
 const GlobalContextProvider = ({ children }: { children: ReactNode }) => {
-  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+  const [allNotes, setAllNotes] = useState<Note[]>([]);
+
+  function addNote(note: Note) {
+    setAllNotes((prev) => [...prev, note]);
+  }
+
+  function deleteNote(noteId: number) {
+    const filteredArray = allNotes.filter((note: Note) => note.id !== noteId);
+    setAllNotes(filteredArray);
+  }
+
+  function updateNote(newValue: string, noteId: number) {
+    const matchingNote = allNotes.find((prev: Note) => prev.id === noteId);
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{ allNotes, setAllNotes, addNote, deleteNote, updateNote }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 };
 
 export { GlobalContext, GlobalContextProvider };
--- google_notes/src/Context/types/contextTypes.ts
+++ google_notes/src/Context/types/contextTypes.ts
@@ -1,1 +1,16 @@
-export interface GlobalContextType {}
+import { Dispatch, SetStateAction } from "react";
+
+const exampleNote = {
+  id: 3,
+  title: "lorem ipsum",
+};
+
+export type Note = typeof exampleNote;
+
+export interface GlobalContextType {
+  allNotes: Note[];
+  setAllNotes: Dispatch<SetStateAction<Note[]>>;
+  addNote: (note: Note) => void;
+  deleteNote: (noteId: number) => void;
+  updateNote: (newValue: string, noteId: number) => void;
+}
